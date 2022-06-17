6400e92ed3de7bbbcf8344e4a86e7b7d05f049d2
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-17 14:17:36 +0200

    exercise 2 solved

--- expert.php
+++ expert.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+
 function new_exercise() {
     $x = (int)1;
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
@@ -9,4 +15,14 @@ function new_exercise() {
 }
 
 new_exercise();
+
+
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
 
