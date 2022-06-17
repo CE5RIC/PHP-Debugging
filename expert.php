60a8b28fd9207531e4abccf875994e50f4fb47bc
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-17 11:59:20 +0200

    First exercise solved

---
+++ expert.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+function new_exercise() {
+    $x = (int)1;
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+
+}
+
+new_exercise();
+
