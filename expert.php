ca44a366197c4241653ebe1dde94eba259506d13
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-17 15:32:37 +0200

    exercise 4 solved

--- expert.php
+++ expert.php
@@ -24,7 +24,7 @@ new_exercise();
 $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 $monday = $week[0];
 
-//echo $monday;
+echo $monday;
 
 
 
@@ -33,4 +33,20 @@ $monday = $week[0];
 
 $str = 'Debugged ! Also very fun';
 echo substr($str, 0, 25);
+
+
+
+
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
+
+foreach($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
+
+
 
